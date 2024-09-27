9db5e0025b17a905b2eb40964ce76329c12a4df3
Gréczi-Zsoldos Miklós <miklos.greczi.zsoldos@gmail.com>
2024-09-27 15:41:10 +0200

    build: created Spring Boot project

---
+++ sudoku/src/test/java/hu/unideb/inf/sudoku/SudokuApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.sudoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
