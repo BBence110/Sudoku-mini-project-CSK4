b9c7aabfded06a2a0929ff754d64f57ce41c9591
balaz <balazsi.bence110@gmail.com>
2024-09-29 20:14:28 +0200

    Javadoc to the remaining functions done

--- sudoku/src/main/java/hu/unideb/inf/sudoku/service/SudokuService.java
+++ sudoku/src/main/java/hu/unideb/inf/sudoku/service/SudokuService.java
@@ -10,16 +10,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class SudokuService {
 
+    /**
+     * This is a repository for accessing and managing data.
+     */
     private final SudokuRepository repository;
 
+    /**
+     * Creates a new SudokuService with the given repository.
+     *
+     * @param repository This is used for data access.
+     */
     public SudokuService(SudokuRepository repository) {
         this.repository = repository;
     }
 
+    /**
+     * Retrives a SudokuEntity by its ID.
+     *
+     * @param sudId ID of the sudoku board.
+     * @return the SudokuEntity if found, or null.
+     */
     public SudokuEntity getSudokuBoardById(long sudId) {
         return repository.findById(sudId).orElse(null);
     }
 
+    /**
+     * Retrives a random Sudoku board as SudokuDTO.
+     *
+     * @return a randomly selected SudokuDTO, or null if the board not exist.
+     */
     public SudokuDTO getRandomBoard() {
         long idMax = repository.count();
         long rand = (long) (Math.random() * idMax) + 1;
