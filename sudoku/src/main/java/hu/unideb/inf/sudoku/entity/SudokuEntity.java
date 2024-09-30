610d4994bbccd6685373f73d8b61c1fa832513bf
balaz <balazsi.bence110@gmail.com>
2024-09-30 11:16:03 +0200

    equals

--- sudoku/src/main/java/hu/unideb/inf/sudoku/entity/SudokuEntity.java
+++ sudoku/src/main/java/hu/unideb/inf/sudoku/entity/SudokuEntity.java
@@ -25,5 +25,33 @@ public class SudokuEntity {
     @Column(nullable = false, unique = true)
     private String board;
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((board == null) ? 0 : board.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SudokuEntity other = (SudokuEntity) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (board == null) {
+            return other.board == null;
+        } else return board.equals(other.board);
+    }
+
 
 }
